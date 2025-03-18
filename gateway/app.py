ab82a784ed84520d3ae44e7c6363a27e0eece26d
Dennis Heijmans <dheijmans@tudelft.nl>
2025-03-18 22:55:12 +0100

    Implement remote procedure calls

--- gateway/app.py
+++ gateway/app.py
@@ -1,11 +1,21 @@
+import os
 from quart import Quart
+from rpc_client import RpcClient
 
 app = Quart(__name__)
+rpc_client = RpcClient()
 
 
-@app.route("/")
-async def index() -> str:
-    return {"key": "value"}
+@app.before_serving
+async def startup():
+    await rpc_client.connect(os.environ["AMQP_URL"])
+
+
+@app.route("/payment/find_user/<user_id>")
+async def find_user(user_id):
+    response = await rpc_client.call(user_id, "payment_queue")
+    return response
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
