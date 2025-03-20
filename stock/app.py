c89342e2efa445cfefccbb3e305b47ac52da0786
Dimitris Ntatsis <dntatsis@student.tudelft.nl>
2025-03-20 15:57:58 +0100

    Stock connection with gateway

--- stock/app.py
+++ stock/app.py
@@ -4,6 +4,7 @@ import atexit
 import uuid
 
 import redis
+from common.amqp_worker import AMQPWorker
 
 from msgspec import msgpack, Struct
 from flask import Flask, jsonify, abort, Response
@@ -18,6 +19,11 @@ db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
                               password=os.environ['REDIS_PASSWORD'],
                               db=int(os.environ['REDIS_DB']))
 
+
+worker = AMQPWorker(
+    amqp_url=os.environ["AMQP_URL"],
+    queue_name="stock_queue",
+)
 
 def close_db_connection():
     db.close()
@@ -76,31 +82,31 @@ def atomic_update_item(item_id: str, update_func):
 
         except redis.RedisError:
             # Catch any other Redis-related errors and abort with a 400 status
-            abort(400, DB_ERROR_STR)
+            return DB_ERROR_STR, 400
 
         except Exception as e:
             # Catch any other unexpected exceptions to prevent silent failures
-            abort(401, f"Unexpected error: {str(e)}")
+            return f"Unexpected error: {str(e)}", 401
 
 
-@app.post('/item/create/<price>')
-def create_item(price: int):
+@worker.register
+async def create_item(price: int):
     key = str(uuid.uuid4())
     app.logger.debug(f"Item: {key} created")
     value = msgpack.encode(StockValue(stock=0, price=int(price)))
     try:
         db.set(key, value)
     except redis.exceptions.RedisError:
-        return abort(400, DB_ERROR_STR)
+        return DB_ERROR_STR, 400
     except Exception as e:
         # Catch any other unexpected exceptions to prevent silent failures
-        abort(401, f"Unexpected error: {str(e)}")
+        return f"Unexpected error: {str(e)}", 401
 
-    return jsonify({'item_id': key}), 200
+    return {'item_id': key}, 200
 
 
-@app.post('/batch_init/<n>/<starting_stock>/<item_price>')
-def batch_init_users(n: int, starting_stock: int, item_price: int):
+@worker.register
+async def batch_init_users(n: int, starting_stock: int, item_price: int):
     n = int(n)
     starting_stock = int(starting_stock)
     item_price = int(item_price)
@@ -109,51 +115,46 @@ def batch_init_users(n: int, starting_stock: int, item_price: int):
     try:
         db.mset(kv_pairs)
     except redis.exceptions.RedisError:
-        return abort(400, DB_ERROR_STR)
+        return DB_ERROR_STR, 400
     except Exception as e:
         # Catch any other unexpected exceptions to prevent silent failures
-        abort(401, f"Unexpected error: {str(e)}")
+        return f"Unexpected error: {str(e)}", 401
 
 
-    return jsonify({"msg": "Batch init for stock successful"}), 200
+    return {'msg': "Batch init for stock successful"}, 200
 
 
-@app.get('/find/<item_id>')
-def find_item(item_id: str):
+@worker.register
+async def find_item(item_id: str):
     item_entry: StockValue = get_item_from_db(item_id)
-    return jsonify(
-        {
-            "stock": item_entry.stock,
-            "price": item_entry.price
-        }
-    ), 200
+    return {"stock": item_entry.stock,
+            "price": item_entry.price } , 200
 
-
-@app.post('/add/<item_id>/<amount>')
-def add_stock(item_id: str, amount: int):
-    def stock_value_change(item: StockValue) -> StockValue:
+@worker.register
+async def add_stock(item_id: str, amount: int):
+    async def stock_value_change(item: StockValue) -> StockValue:
         item.stock += int(amount)
         return item
     atomic_update_item(item_id,stock_value_change)
     updated_item = get_item_from_db(item_id)
-    return jsonify({"Added": True, "UpdatedStock": updated_item.stock}), 200
+    return {"Added": True, 
+            "UpdatedStock": updated_item.stock
+            }, 200
 
 
-@app.post('/subtract/<item_id>/<amount>')
-def remove_stock(item_id: str, amount: int):
-    def stock_value_change(item: StockValue) -> StockValue:
+@worker.register
+async def remove_stock(item_id: str, amount: int):
+    async def stock_value_change(item: StockValue) -> StockValue:
         if item.stock - int(amount) < 0:
-            abort(400, f"Item: {item_id} has insufficient stock!")
+            return f"{item_id} has insufficient stock!", 400
         item.stock -= int(amount)
         return item
     atomic_update_item(item_id,stock_value_change)
     updated_item = get_item_from_db(item_id)
-    return jsonify({"Removed": True, "UpdatedStock": updated_item.stock}), 200
-
+    return {"Removed": True, 
+            "UpdatedStock": updated_item.stock
+            }, 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
-else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    asyncio.run(worker.start())
+
